981eb85c0cbd9ce5469358d7d83213643238f118
RibaDiba <abrmod74@gmail.com>
2024-10-02 19:40:44 -0400

    updated master code

--- background_master/background_master.ino
+++ background_master/background_master.ino
@@ -3,10 +3,15 @@
 
 #define buttonPin1 18
 #define buttonPin2 26
-#define buttonPin3 27 
+#define buttonPin3 27
 #define buttonPin4 19
 
 #define slave_mac_addr
+
+volatile int buttonState1 = HIGH;
+volatile int buttonState2 = HIGH;
+volatile int buttonState3 = HIGH;
+volatile int buttonState4 = HIGH;
 
 int lastButtonState1 = HIGH;
 int lastButtonState2 = HIGH;
@@ -25,6 +30,23 @@ struct_message buttonMessage;
 void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("Last Packet Send Status: ");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+// not sure if this logic is correct 
+void IRAM_ATTR handleButton1() {
+  buttonState1 = digitalRead(buttonPin1);
+}
+
+void IRAM_ATTR handleButton2() {
+  buttonState2 = digitalRead(buttonPin2);
+}
+
+void IRAM_ATTR handleButton3() {
+  buttonState3 = digitalRead(buttonPin3);
+}
+
+void IRAM_ATTR handleButton4() {
+  buttonState4 = digitalRead(buttonPin4);
 }
 
 void setup() {
@@ -53,14 +75,15 @@ void setup() {
   pinMode(buttonPin2, INPUT_PULLUP);
   pinMode(buttonPin3, INPUT_PULLUP);
   pinMode(buttonPin4, INPUT_PULLUP);
+
+  // atached interrupts here 
+  attachInterrupt(buttonPin1, handleButton1, CHANGE);
+  attachInterrupt(buttonPin2, handleButton2, CHANGE);
+  attachInterrupt(buttonPin3, handleButton3, CHANGE);
+  attachInterrupt(buttonPin4, handleButton4, CHANGE);
 }
 
 void loop() {
-  int buttonState1 = digitalRead(buttonPin1);
-  int buttonState2 = digitalRead(buttonPin2);
-  int buttonState3 = digitalRead(buttonPin3);
-  int buttonState4 = digitalRead(buttonPin4);
-
   if (buttonState1 != lastButtonState1 || buttonState2 != lastButtonState2 || 
       buttonState3 != lastButtonState3 || buttonState4 != lastButtonState4) {
 
@@ -76,12 +99,12 @@ void loop() {
     } else {
       Serial.println("Error sending the data");
     }
+
+    lastButtonState1 = buttonState1;
+    lastButtonState2 = buttonState2;
+    lastButtonState3 = buttonState3;
+    lastButtonState4 = buttonState4;
   }
-
-  lastButtonState1 = buttonState1;
-  lastButtonState2 = buttonState2;
-  lastButtonState3 = buttonState3;
-  lastButtonState4 = buttonState4;
-
-  delay(100);
+  
+  delay(100); // Small delay to prevent spamming
 }
