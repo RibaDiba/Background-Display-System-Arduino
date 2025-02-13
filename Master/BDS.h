515df2aba5ab44d5184c0f0ab6cb39931699ec3c
Abir Modak <abrmod74@gmail.com>
2025-02-12 21:28:39 -0500

    finished loop

--- Master/BDS.h
+++ Master/BDS.h
@@ -3,16 +3,18 @@
 class BDS {
     public:
     int position;
-    uint8_t esp32MAC;
+    uint8_t *esp32MAC;
     bool active;
 
-    BDS(int position, uint8_t esp32MAC, bool active);
+    BDS(int position, uint8_t *esp32MAC, bool active);
     void setPosition(int newPos);
     void setActive(bool newActive);
-    uint8_t getBroadcastAddr();
+    bool getActive();
+    int getPosition();
+    uint8_t* getBroadcastAddr();
 };
 
-BDS::BDS(int position, uint8_t esp32MAC, bool active) 
+BDS::BDS(int position, uint8_t *esp32MAC, bool active) 
     : position(position), esp32MAC(esp32MAC), active(active) {
     
     Serial.println(position);
@@ -22,10 +24,18 @@ void BDS::setPosition(int newPos) {
     position = newPos;
 }
 
+int BDS::getPosition() {
+    return position;
+}
+
 void BDS::setActive(bool newActive) {
     active = newActive;
 }
 
-uint8_t BDS::getBroadcastAddr() {
+bool BDS::getActive() {
+    return active;
+}
+
+uint8_t* BDS::getBroadcastAddr() {
     return esp32MAC;
 }
--- Master/Master.ino
+++ Master/Master.ino
@@ -2,14 +2,17 @@
 #include <WiFi.h>
 #include "BDS.h"
 
-#define maxSpeed 100 
-#define maxAccel 200
+int maxSpeed = 100;
+int maxAccel = 200;
 
 // bds, switch 1, switch 2, switch 3
-int switchPins[4] = {13, 26, 32, 35};
+int switchPins[4] = {13, 35, 32, 26};
 
 // scene 1, scene 2, scene 3
-int positions[3] = {0, 540*16, 1080*16};
+int positions[3] = {0, 540 * 16, 1080 * 16};
+
+// first 3 are for scenes and 14 and 23 are for switching bds indicator 
+int ledPins[5] = {34, 33, 27, 14, 23};
 
 // this line is to make sure interrupts are handled correctly 
 portMUX_TYPE synch = portMUX_INITIALIZER_UNLOCKED;
@@ -21,37 +24,49 @@ typedef struct message {
     int maxSpeed;
 } message;
 
-message broadcastMessage;
-broadcastMessage.maxAccel = maxAccel;
-broadcastMessage.maxSpeed = maxSpeed;
-broadcastMessage.position = 0;
+volatile message broadcastMessage = {0, maxAccel, maxSpeed};
 
 void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-    Serial.print("Last Packet Send Status: ");
-    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+    // Serial.print("Last Packet Send Status: ");
+    // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void initPins() {
+    for (int i = 0; i < 4; i++) {
+        pinMode(switchPins[i], INPUT_PULLUP);
+    }
+    for (int i = 0; i < 5; i++) {
+        pinMode(ledPins[i], OUTPUT);
+    }
 }
 
 // declare BDS objects
-BDS system1 = BDS(0, { 0xcc, 0xdb, 0xa7, 0x3e, 0xe7, 0xec }, true);
-BDS system2 = BDS(0, { 0x08, 0xa6, 0xf7, 0xbc, 0x7f, 0x64 }, false);
+BDS system1 = BDS(0, (uint8_t[]){0xcc, 0xdb, 0xa7, 0x3e, 0xe7, 0xec}, false);
+BDS system2 = BDS(0, (uint8_t[]){0x08, 0xa6, 0xf7, 0xbc, 0x7f, 0x64}, false);
+
 
 // interrupt functions here
 void IRAM_ATTR handleButton(int Pos) {
     portENTER_CRITICAL(&synch);
     broadcastMessage.position = Pos;
+    Serial.print("Button Pressed: ");
+    Serial.println(Pos);
     portEXIT_CRITICAL(&synch);
 }
 
 void IRAM_ATTR handleSwitch() {
     int switchState = digitalRead(switchPins[0]);
+    system1.setActive(switchState == HIGH);
+    system2.setActive(switchState == LOW);
+}
 
-    if (switchState == HIGH) {
-        system1.setActive(true);
-        system2.setActive(false);
-    } else {
-        system1.setActive(false);
-        system2.setActive(true);
-    }
+void initBDSLed() {
+    int switchState = digitalRead(switchPins[0]);
+    system1.setActive(switchState == HIGH);
+    system2.setActive(switchState == LOW);
+
+    system1.getActive() ? (digitalWrite(ledPins[3], HIGH), digitalWrite(ledPins[4], LOW)) 
+    : (digitalWrite(ledPins[3], LOW), digitalWrite(ledPins[4], HIGH));
 }
 
 void setup() {
@@ -60,49 +75,67 @@ void setup() {
     WiFi.disconnect();
 
     if (esp_now_init() != ESP_OK) {
-        Serial.println("Err when init espnow");
+        Serial.println("Error initializing ESP-NOW");
         return;
     }
+    initPins();
+    initBDSLed();
 
     esp_now_register_send_cb(onDataSent);
 
-    esp_now_peer_info_t peerInfo1 = {};
-    memcpy(peerInfo1.peer_addr, system1.getBroadcastAddr(), 6);
-    peerInfo1.channel = 0;
-    peerInfo1.encrypt = false;
-    if (esp_now_add_peer(&peerInfo1) != ESP_OK) {
+    esp_now_peer_info_t peerInfo = {};
+    memcpy(peerInfo.peer_addr, system1.getBroadcastAddr(), 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+    if (esp_now_add_peer(&peerInfo) != ESP_OK) {
         Serial.println("Failed to add peer 1");
         return;
     }
 
-    esp_now_peer_info_t peerInfo2 = {};
-    memcpy(peerInfo2.peer_addr, system2.getBroadcastAddr(), 6);
-    peerInfo2.channel = 0;
-    peerInfo2.encrypt = false;
-    if (esp_now_add_peer(&peerInfo2) != ESP_OK) {
+    memcpy(peerInfo.peer_addr, system2.getBroadcastAddr(), 6);
+    if (esp_now_add_peer(&peerInfo) != ESP_OK) {
         Serial.println("Failed to add peer 2");
         return;
     }
 
     // interrupts for buttons
-    attatchInterrupt(digitalPinToInterrupt(switchPins[1]), [] {handleButton(positions[0]);}, FALLING)
-    attatchInterrupt(digitalPinToInterrupt(switchPins[2]), [] {handleButton(positions[1]);}, FALLING)
-    attatchInterrupt(digitalPinToInterrupt(switchPins[3]), [] {handleButton(positions[2]);}, FALLING)
+    attachInterrupt(digitalPinToInterrupt(switchPins[1]), [] { handleButton(positions[0]); }, FALLING);
+    attachInterrupt(digitalPinToInterrupt(switchPins[2]), [] { handleButton(positions[1]); }, FALLING);
+    attachInterrupt(digitalPinToInterrupt(switchPins[3]), [] { handleButton(positions[2]); }, FALLING);
 
     // interrupt for bds switch 
     attachInterrupt(digitalPinToInterrupt(switchPins[0]), handleSwitch, CHANGE);
 }
 
-void loop() {
-// TODO: finish loop here
-
+void writePosLed(int pos) {
+    if (pos == positions[0]) {
+        digitalWrite(ledPins[0], HIGH);
+        digitalWrite(ledPins[1], LOW);
+        digitalWrite(ledPins[2], LOW);
+    }
+    else if (pos == positions[1]) {
+        digitalWrite(ledPins[0], LOW);
+        digitalWrite(ledPins[1], HIGH);
+        digitalWrite(ledPins[2], LOW);
+    }
+    else if (pos == positions[2]) {
+        digitalWrite(ledPins[0], LOW);
+        digitalWrite(ledPins[1], LOW);
+        digitalWrite(ledPins[2], HIGH);
+    }
 }
 
 
+void loop() {
+    uint8_t *broadcastAddress = system1.getActive() ? system1.getBroadcastAddr() : system2.getBroadcastAddr();
+    // Serial.println(system1.getActive() ? "Sending to System 1" : "Sending to System 2");
+    system1.getActive() ? (digitalWrite(ledPins[3], HIGH), digitalWrite(ledPins[4], LOW), writePosLed(system1.getPosition())) 
+        : (digitalWrite(ledPins[3], LOW), digitalWrite(ledPins[4], HIGH), writePosLed(system2.getPosition()));
 
+    Serial.println(system1.getPosition());
 
-
-
-
-
-
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&broadcastMessage, sizeof(broadcastMessage));
+    if (result == ESP_OK) {
+        // Serial.println("Sent with Success");
+    }
+}
