7d43c05f9ad40dbe7188a8c36f85678e0b2098a1
Abir Modak <abrmod74@gmail.com>
2025-03-11 16:53:00 -0400

    finished new code

--- Master/BDS.h
+++ Master/BDS.h
@@ -1,41 +1,82 @@
+#pragma once
 #include <cstdint>
+
+typedef struct stepperMotor {
+    int position[3];
+    int maxSpeed;
+    int maxAccel;
+} stepperMotor;
+
+typedef struct boardData {
+    int s1Position;
+    int s2Position;
+    
+    int s1MaxSpeed;
+    int s2MaxSpeed;
+
+    int s1MaxAccel;
+    int s2MaxAccel;
+} boardData;
 
 class BDS {
     public:
-    int position;
-    uint8_t *esp32MAC;
-    bool active;
+        uint8_t *esp32MAC;
+        bool active;
+        stepperMotor s1;
+        stepperMotor s2;
 
-    BDS(int position, uint8_t *esp32MAC, bool active);
-    void setPosition(int newPos);
-    void setActive(bool newActive);
-    bool getActive();
-    int getPosition();
-    uint8_t* getBroadcastAddr();
-};
+        boardData dataSend;
 
-BDS::BDS(int position, uint8_t *esp32MAC, bool active) 
-    : position(position), esp32MAC(esp32MAC), active(active) {
+        BDS(uint8_t *esp32MAC, stepperMotor s1, stepperMotor s2, bool active);
     
-    Serial.println(position);
+        void setPositionIndex(int newIndex);
+        void setBroadData();
+        boardData getBroadData();
+        bool getActive();
+        uint8_t* getBroadcastAddr();
+        int getPositionIndex();
+        void setActive(bool active);
+    private:
+        int positionIndex = 0;
 }
 
-void BDS::setPosition(int newPos) {
-    position = newPos;
+BDS::BDS(uint8_t *esp32MAC, stepperMotor s1, stepperMotor s2, bool active) 
+    : esp32MAC(esp32MAC), s1(s1), s2(s2), active(active) {
+
 }
 
-int BDS::getPosition() {
-    return position;
+void BDS::setBroadData() {
+    dataSend.s1Position = s1.position[positionIndex];
+    dataSend.s2Position = s2.position[positionIndex];
+
+    dataSend.s1MaxSpeed = s1.maxSpeed;
+    dataSend.s2MaxSpeed = s2.maxSpeed;
+
+    dataSend.s1MaxAccel = s1.maxAccel;
+    dataSend.s2MaxAccel = s2.maxAccel;
+
 }
 
-void BDS::setActive(bool newActive) {
-    active = newActive;
+int BDS::getPositionIndex() {
+    return positionIndex;
+}
+
+uint8_t* BDS::getBroadcastAddr() {
+    return esp32MAC;
+}
+
+boardData BDS::getBroadData() {
+    return dataSend;
+}
+
+void BDS::setPositionIndex(int newIndex) {
+    positionIndex = newIndex;
 }
 
 bool BDS::getActive() {
     return active;
 }
 
-uint8_t* BDS::getBroadcastAddr() {
-    return esp32MAC;
+void BDS::setActive(bool active) {
+    this->active = active;
 }
--- Master/Master.ino
+++ Master/Master.ino
@@ -2,14 +2,14 @@
 #include <WiFi.h>
 #include "BDS.h"
 
-int maxSpeed = 400;
-int maxAccel = 50;
+// define structs ({arr, of, positions}, maxSpeed, maxAccel)
+stepperMotor sys1S1 = {{300, 600, 1200}, 400, 200};
+stepperMotor sys1S2 = {{300, 600, 1200}, 400, 200};
+stepperMotor sys2S1 = {{300, 600, 1200}, 400, 200};
+stepperMotor sys2S2 = {{300, 600, 1200}, 400, 200};
 
 // bds, switch 1, switch 2, switch 3  
 int switchPins[4] = {13, 35, 32, 26};
-
-// scene 1, scene 2, scene 3
-int positions[3] = {0, 66.67 * 16 * 4, 133.33 * 16 * 4};
 
 // first 3 are for scenes and 14 and 23` are for switching bds indicator 
 int ledPins[5] = {22, 33, 27, 14, 23};
@@ -17,21 +17,18 @@ int ledPins[5] = {22, 33, 27, 14, 23};
 // this line is to make sure interrupts are handled correctly 
 portMUX_TYPE synch = portMUX_INITIALIZER_UNLOCKED;
 
-// struct for message sent to BDS
-typedef struct message {
-    volatile int position;
-    int maxAccel;
-    int maxSpeed;
-    uint8_t* address;
-} message;
-
-volatile message broadcastMessage = {0, maxAccel, maxSpeed};
-
+// callback when data has been sent out
 void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
     Serial.print("Last Packet Send Status: ");
     Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-    Serial.println(broadcastMessage.position);
 }
+
+// declare bds objects 
+BDS system1 = BDS((uint8_t[]){0x08, 0xa6, 0xf7, 0xb1, 0x10, 0x90}, sys1S1, sys1S2, false);
+BDS system2 = BDS((uint8_t[]){0x08, 0xa6, 0xf7, 0xbc, 0x7f, 0x64}, sys2S1, sys2S2, false);
+
+// declare message variable for broadcast data
+boardData message;
 
 void initPins() {
     for (int i = 0; i < 4; i++) {
@@ -41,28 +38,6 @@ void initPins() {
         pinMode(ledPins[i], OUTPUT);
     }
 }
-// sys1 = 08:a6:f7:b1:10:90
-// declare BDS objects
-BDS system1 = BDS(0, (uint8_t[]){0x08, 0xa6, 0xf7, 0xb1, 0x10, 0x90}, false);
-BDS system2 = BDS(0, (uint8_t[]){0x08, 0xa6, 0xf7, 0xbc, 0x7f, 0x64}, false);
-
-
-// interrupt functions here
-void IRAM_ATTR handleButton(int Pos) {
-    portENTER_CRITICAL(&synch);
-    if (system1.getActive() == true) {
-        system1.setPosition(Pos);
-    } else {
-        system2.setPosition(Pos);
-    }
-    portEXIT_CRITICAL(&synch);
-}
-
-void IRAM_ATTR handleSwitch() {
-    int switchState = digitalRead(switchPins[0]);
-    system1.setActive(switchState == HIGH);
-    system2.setActive(switchState == LOW);
-}
 
 void initBDSLed() {
     int switchState = digitalRead(switchPins[0]);
@@ -71,6 +46,42 @@ void initBDSLed() {
 
     system1.getActive() ? (digitalWrite(ledPins[3], HIGH), digitalWrite(ledPins[4], LOW)) 
     : (digitalWrite(ledPins[3], LOW), digitalWrite(ledPins[4], HIGH));
+}
+
+void IRAM_ATTR handleSwitch() {
+    int switchState = digitalRead(switchPins[0]);
+    system1.setActive(switchState == HIGH);
+    system2.setActive(switchState == LOW);
+}
+
+
+void IRAM_ATTR handleButton(int index) {
+    portENTER_CRITICAL(&synch);
+    if (system1.getActive()) {
+        system1.setPositionIndex(index);
+    } else {
+        system2.setPositionIndex(index);
+    }
+    portEXIT_CRITICAL(&synch);
+}
+
+// update leds based on position
+void writePosLed(int index) {
+    if (index == 0) {
+        digitalWrite(ledPins[0], HIGH);
+        digitalWrite(ledPins[1], LOW);
+        digitalWrite(ledPins[2], LOW);
+    }
+    else if (index == 1) {
+        digitalWrite(ledPins[0], LOW);
+        digitalWrite(ledPins[1], HIGH);
+        digitalWrite(ledPins[2], LOW);
+    }
+    else if (index == 2) {
+        digitalWrite(ledPins[0], LOW);
+        digitalWrite(ledPins[1], LOW);
+        digitalWrite(ledPins[2], HIGH);
+    }
 }
 
 void setup() {
@@ -103,62 +114,37 @@ void setup() {
     }
 
     // interrupts for buttons
-    attachInterrupt(digitalPinToInterrupt(switchPins[1]), [] { handleButton(positions[0]); }, FALLING);
-    attachInterrupt(digitalPinToInterrupt(switchPins[2]), [] { handleButton(positions[1]); }, FALLING);
-    attachInterrupt(digitalPinToInterrupt(switchPins[3]), [] { handleButton(positions[2]); }, FALLING);
+    attachInterrupt(digitalPinToInterrupt(switchPins[1]), [] { handleButton(0); }, FALLING);
+    attachInterrupt(digitalPinToInterrupt(switchPins[2]), [] { handleButton(1); }, FALLING);
+    attachInterrupt(digitalPinToInterrupt(switchPins[3]), [] { handleButton(2); }, FALLING);
 
     // interrupt for bds switch 
     attachInterrupt(digitalPinToInterrupt(switchPins[0]), handleSwitch, CHANGE);
 }
 
-void writePosLed(int pos) {
-    if (pos == positions[0]) {
-        digitalWrite(ledPins[0], HIGH);
-        digitalWrite(ledPins[1], LOW);
-        digitalWrite(ledPins[2], LOW);
-    }
-    else if (pos == positions[1]) {
-        digitalWrite(ledPins[0], LOW);
-        digitalWrite(ledPins[1], HIGH);
-        digitalWrite(ledPins[2], LOW);
-    }
-    else if (pos == positions[2]) {
-        digitalWrite(ledPins[0], LOW);
-        digitalWrite(ledPins[1], LOW);
-        digitalWrite(ledPins[2], HIGH);
-    }
-}
-
-
 void loop() {
-    // Serial.println(system1.getActive() ? "Sending to System 1" : "Sending to System 2");
-    // system1.getActive() ? (digitalWrite(ledPins[3], HIGH), digitalWrite(ledPins[4], LOW), writePosLed(system1.getPosition())) 
-    //     : (digitalWrite(ledPins[3], LOW), digitalWrite(ledPins[4], HIGH), writePosLed(system2.getPosition()));
-
-    // bool isMatching = system1.getActive() 
-    // ? (broadcastMessage.position == system1.getPosition()) 
-    // : (broadcastMessage.position == system2.getPosition());
-
-    // Serial.println(broadcastMessage.position);
-
     if (system1.getActive()) {
         digitalWrite(ledPins[3], HIGH);
         digitalWrite(ledPins[4], LOW);
-        broadcastMessage.address = system1.getBroadcastAddr();
-        broadcastMessage.position = system1.getPosition();
-        writePosLed(system1.getPosition());
+        system1.setBroadData();
+        message = system1.getBroadData();
+        writePosLed(system1.getPositionIndex());
         // printf("Sending to System 1 with: %d\n", broadcastMessage.position);
+        esp_err_t result = esp_now_send(system1.getBroadcastAddr(), (uint8_t *)&message, sizeof(message));
+        if (result == ESP_OK) {
+            Serial.print("Sent with Success");
+        }
     } else {
         digitalWrite(ledPins[3], LOW);
         digitalWrite(ledPins[4], HIGH);
-        writePosLed(system2.getPosition());
-        broadcastMessage.address = system2.getBroadcastAddr();
-        broadcastMessage.position = system2.getPosition();
-        // printf("Sending to System 2 with: %d\n", broadcastMessage.position);
+        system2.setBroadData();
+        message = system2.getBroadData();
+        writePosLed(system2.getPositionIndex());
+        // printf("Sending to System 1 with: %d\n", broadcastMessage.position);
+        esp_err_t result = esp_now_send(system2.getBroadcastAddr(), (uint8_t *)&message, sizeof(message));
+        if (result == ESP_OK) {
+            Serial.print("Sent with Success");
+        }
     }    
-
-    esp_err_t result = esp_now_send(broadcastMessage.address, (uint8_t *)&broadcastMessage, sizeof(broadcastMessage));
-    if (result == ESP_OK) {
-        Serial.print("Sent with Success");
-    }
 }
+
